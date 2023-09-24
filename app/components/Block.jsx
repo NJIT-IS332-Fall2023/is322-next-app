41e089bdeb6790974fcb14398c1db437cd2742c5
Srihari Rao <harirao3@gmail.com>
2023-09-24 10:52:25 -0400

    feat: refactor out <Block/> and <Text/>
    sample react components, introdcution to react week 1

---
+++ app/components/Block.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+// /TODO - add href prop
+export const Block = ({headerText, subtitleText, linkTo = 'www.google.com'}) =>{
+    return (
+        <>
+            <a
+            href={linkTo}
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+                <h2 className={`mb-3 text-2xl font-semibold`}>
+                    {headerText}{' '}
+                    <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                    -&gt;
+                    </span>
+                </h2>
+                <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+                    {subtitleText}
+                </p>
+            </a>
+        </>
+    )
+}
+
---
+++ app/components/Text.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+// interface TextProps  {
+//     label: String
+//     count: Number,
+// }
+
+export const Text = ({label}) => {
+    return (
+        <p>{label}</p>
+    )
+}
+
+// export default Text;
