6da1d0c9c0b08efd1a2680a5035d83f764ca7ed8
Srihari Rao <harirao3@gmail.comm>
2023-09-29 18:38:05 -0400

    fix: data fetching, headless etc

---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import BottomNav from '../components/BottomNav';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-200">
+      <h1 className="text-center text-2xl p-4">Home Page</h1>
+      <BottomNav />
+    </div>
+  );
+}
